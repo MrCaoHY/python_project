b5a01bc21427ffc376a7144439b5eb0feee1d9b9
caohaiyangDesktop <caohaiyang666888@outlook.com>
2026-01-30 03:14:11 +0800

    新建了设置类和项目主程序文件

---
+++ alien/aline_invasion.py
@@ -0,0 +1,24 @@
+import sys
+import pygame
+from setting import Setting
+class AlienInvasion:
+    """管理游戏资源和行为的类"""
+    def __init__(self):
+        pygame.init()
+        self.setting = Setting()
+        self.screen = pygame.display.set_mode((self.setting.screen_width, self.setting.screen_height))
+        pygame.display.set_caption("Alien Invasion")
+
+
+    def run_game(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            # 每次循环重绘屏幕
+            self.screen.fill(self.setting.bg_color)
+            pygame.display.flip()
+
+if __name__ == "__main__":
+    alien = AlienInvasion()
+    alien.run_game()
---
+++ alien/setting.py
@@ -0,0 +1,7 @@
+class Setting:
+    #存储游戏中所有设置的类
+    def __init__(self):
+        # 屏幕设置
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230,230,230)
