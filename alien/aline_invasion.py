7542a4702962411c5ab6f18586f38c3eb1aa1e41
Laurence Konopelski <caohaiyang666888@gmail.com>
2026-01-30 19:18:14 +0800

    创建飞船类并且在屏幕上绘制

--- alien/aline_invasion.py
+++ alien/aline_invasion.py
@@ -1,5 +1,7 @@
 import sys
 import pygame
+
+from alien.ship import Ship
 from setting import Setting
 class AlienInvasion:
     """管理游戏资源和行为的类"""
@@ -8,7 +10,7 @@ class AlienInvasion:
         self.setting = Setting()
         self.screen = pygame.display.set_mode((self.setting.screen_width, self.setting.screen_height))
         pygame.display.set_caption("Alien Invasion")
-
+        self.ship = Ship(self)
 
     def run_game(self):
         while True:
@@ -17,6 +19,7 @@ class AlienInvasion:
                     sys.exit()
             # 每次循环重绘屏幕
             self.screen.fill(self.setting.bg_color)
+            self.ship.blitme()
             pygame.display.flip()
 
 if __name__ == "__main__":
